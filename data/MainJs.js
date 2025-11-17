ddbfbd40200dca5cdbf5a7a6152e450405cdbe57
ToxicWixorg <toxicwix@gmail.com>
2025-11-17 11:39:28 +0330

    Update MainJs.js

--- data/MainJs.js
+++ data/MainJs.js
@@ -33,7 +33,7 @@ let slideindex = 0;
 let slides = [];
 
 function initgamedatas() {
-  fetch("asset/game_data/GameData.json")
+  fetch("https://toxicwixorg.github.io/NovelgamTeam/asset/game_data/GameData.json")
     .then((response) => response.json())
     .then((games) => {
       if (games.length > 0) {
@@ -108,6 +108,7 @@ function randintnum(a, b) {
   b++;
   return Math.floor(Math.random() * (b - a) + a);
 }
+
 
 
 
